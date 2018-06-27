1bb5026a1ca5fce2ac32b7f3803b589a302b3068
Karthik M S <ms18karthik@gmail.com>
2018-06-27 16:07:50 +0530

    Connecting to ChatKit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import Chatkit from '@pusher/chatkit';
 import MessageList from './components/MessageList';
+import { tokenUrl, instanceLocator } from './config';
 
 
 class App extends React.Component {
+
+  componentDidMount() {
+    var chatManager = new Chatkit.ChatManager({
+        instanceLocator,
+        userId: 'karthik',
+        tokenProvider: new Chatkit.TokenProvider({
+          url: tokenUrl
+        })
+    })
+
+      chatManager.connect()
+      .then(currentUser => {
+        currentUser.subscribeToRoom({
+          roomId: 10387571,
+          hooks: {
+            onNewMessage: message => {
+              console.log('message.text: ', message.text);
+            }
+          }
+        });
+      })
+  }
+
   render() {
     return(
       <MessageList />
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/ef7d78b0-6aee-407b-80f5-36ff2f538419/token";
+
+const instanceLocator = "v1:us1:ef7d78b0-6aee-407b-80f5-36ff2f538419";
+
+export { tokenUrl, instanceLocator }
